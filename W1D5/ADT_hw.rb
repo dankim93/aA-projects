a31fe2cc69d0f063b27691df9a589d4871c27ba8
dankim93 <dan_kim93@hotmail.com>
2016-11-17 23:00:04 -0800

    create stack class

---
+++ W1D5/ADT_hw.rb
@@ -0,0 +1,25 @@
+class Stack
+   def initialize
+     # create ivar to store stack here!
+     @stack = []
+
+   end
+
+   def add(el)
+     # adds an element to the stack
+     @stack << el
+   end
+
+   def remove
+     # removes one element from the stack
+     @stack.pop
+   end
+
+   def show
+     # return a copy of the stack
+     @stack.dup
+   end
+ end
+stack = Stack.new
+5.times { p stack.add("a") }
+p stack.show

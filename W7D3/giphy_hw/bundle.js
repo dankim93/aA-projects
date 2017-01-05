a95f5fa3f6d59a20d6294da311c03e3638961946
dankim93 <dan_kim93@hotmail.com>
2017-01-04 17:20:23 -0800

    Add index, search.jsx

--- W7D3/giphy_hw/bundle.js
+++ W7D3/giphy_hw/bundle.js
@@ -22226,6 +22226,12 @@
 
 	'use strict';
 	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
 	var _react = __webpack_require__(1);
 	
 	var _react2 = _interopRequireDefault(_react);
@@ -22233,8 +22239,74 @@
 	var _giphys_index = __webpack_require__(206);
 	
 	var _giphys_index2 = _interopRequireDefault(_giphys_index);
-
+	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var GiphysSearch = function (_Component) {
+	  _inherits(GiphysSearch, _Component);
+	
+	  function GiphysSearch() {
+	    _classCallCheck(this, GiphysSearch);
+	
+	    var _this = _possibleConstructorReturn(this, (GiphysSearch.__proto__ || Object.getPrototypeOf(GiphysSearch)).call(this));
+	
+	    _this.state = { searchTerm: "golden retriever" };
+	    _this.handleChange = _this.handleChange.bind(_this);
+	    _this.handleSubmit = _this.handleSubmit.bind(_this);
+	    return _this;
+	  }
+	
+	  _createClass(GiphysSearch, [{
+	    key: 'componentDidMount',
+	    value: function componentDidMount() {
+	      this.props.fetchSearchGiphys("golden+retriever");
+	    }
+	  }, {
+	    key: 'handleChange',
+	    value: function handleChange(e) {
+	      this.setState({ searchTerm: e.currentTarget.value });
+	    }
+	  }, {
+	    key: 'handleSubmit',
+	    value: function handleSubmit(e) {
+	      e.preventDefault();
+	      var searchTerm = this.state.searchTerm.split(" ").join("+");
+	      this.props.fetchSearchGiphys(searchTerm);
+	    }
+	  }, {
+	    key: 'render',
+	    value: function render() {
+	      var giphys = this.props.giphys;
+	
+	
+	      return _react2.default.createElement(
+	        'div',
+	        null,
+	        _react2.default.createElement(
+	          'form',
+	          { className: 'search-bar' },
+	          _react2.default.createElement('input', { value: this.state.searchTerm, onChange: this.handleChange }),
+	          _react2.default.createElement(
+	            'button',
+	            { type: 'submit', onClick: this.handleSubmit },
+	            'Search Giphy'
+	          )
+	        ),
+	        _react2.default.createElement(_giphys_index2.default, { giphys: giphys })
+	      );
+	    }
+	  }]);
+	
+	  return GiphysSearch;
+	}(_react.Component);
+	
+	exports.default = GiphysSearch;
 
 /***/ },
 /* 206 */
@@ -22242,11 +22314,68 @@
 
 	'use strict';
 	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
 	var _react = __webpack_require__(1);
 	
 	var _react2 = _interopRequireDefault(_react);
-
+	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var giphyIndexItems = function giphyIndexItems(giphys) {
+	  if (giphys) {
+	    return giphys.map(function (giphy, idx) {
+	      return _react2.default.createElement(
+	        'li',
+	        { key: idx, className: 'giphy-li' },
+	        _react2.default.createElement('img', { src: giphy.images.fixed_height.url })
+	      );
+	    });
+	  } else {
+	    return _react2.default.createElement('div', null);
+	  }
+	};
+	
+	var GiphysIndex = function (_Component) {
+	  _inherits(GiphysIndex, _Component);
+	
+	  function GiphysIndex() {
+	    _classCallCheck(this, GiphysIndex);
+	
+	    return _possibleConstructorReturn(this, (GiphysIndex.__proto__ || Object.getPrototypeOf(GiphysIndex)).apply(this, arguments));
+	  }
+	
+	  _createClass(GiphysIndex, [{
+	    key: 'render',
+	    value: function render() {
+	      var giphys = this.props.giphys;
+	
+	      return _react2.default.createElement(
+	        'div',
+	        null,
+	        _react2.default.createElement(
+	          'ul',
+	          null,
+	          giphyIndexItems(giphys)
+	        )
+	      );
+	    }
+	  }]);
+	
+	  return GiphysIndex;
+	}(_react.Component);
+	
+	exports.default = GiphysIndex;
 
 /***/ }
 /******/ ]);
